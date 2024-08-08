c891f92ee454b2aeaf97efd8b4fedd3a85c4fd43
Gustavo Ceballos Miranda <gceballos6@ucol.mx>
2024-08-08 17:36:50 -0600

    Deploy website - based on a3b85f4cd648bbb2dad7403457895f17346ffb8f

---
+++ assets/js/01d604ba.2b6eec66.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_classic_typescript=self.webpackChunkdocusaurus_classic_typescript||[]).push([[927],{1139:s=>{s.exports=JSON.parse('{"metadata":{"permalink":"/WikiTest/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.c7650235.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_classic_typescript=self.webpackChunkdocusaurus_classic_typescript||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/WikiTest/blog","blogTitle":"Blog"}')}}]);
